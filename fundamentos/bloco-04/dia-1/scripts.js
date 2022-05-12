f2a40d2d85fe6318f426d106b020386220839804
Vinicius Soares <viniciussoares72943@gmail.com>
2022-05-12 18:48:05 -0300

    Exercicios feitos 1 e 2

---
+++ fundamentos/bloco-04/dia-1/scripts.js
@@ -0,0 +1,22 @@
+//EXERCICIO 1
+
+ const a = 10;
+ const b = 2;
+
+ console.log (a + b)
+ console.log (a - b)
+ console.log (a * b)
+ console.log (a / b)
+ console.log (a % b)
+ 
+//EXERCICIO 2
+
+ if (a > b ) {
+  console.log ("variacao maior a")
+ }
+ else{
+  console.log ("variacao maior b")
+ } 
+
+//EXERCICIO 3
+
