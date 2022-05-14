b3ec8110942291c9122588c659f9018bf696000c
Vinicius Soares <viniciussoares72943@gmail.com>
2022-05-14 18:09:01 -0300

    7 exercicio feito

--- fundamentos/bloco-04/dia-1/scripts.js
+++ fundamentos/bloco-04/dia-1/scripts.js
@@ -62,7 +62,7 @@
  }
  */
 //EXERCICIO 6
-
+/* 
 let peça ="bispo"
 
 if (peça =="BISPO".toLowerCase()) {
@@ -71,6 +71,31 @@ if (peça =="BISPO".toLowerCase()) {
 else{
    console.log ("ERRO verifique se a peça esta correta")
 }
+ */
 
+//EXERCICIOS 7
+
+ let nota = 95
+
+ if (nota >= 90){
+   console.log("A")
+ }
+ else if (nota >= 80){
+   console.log("B")
+ }
+ else if (nota >= 70){
+   console.log("C")
+ }
+ else if (nota >= 60){
+   console.log("D")
+ }
+ else if (nota >= 50){
+   console.log("E")
+ }
+ else{
+   console.log("F")
+ }
+
+ 
 
 
