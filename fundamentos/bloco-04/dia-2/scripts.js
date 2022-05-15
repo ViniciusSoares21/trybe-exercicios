e077a9dd51b765e9e879297b22f6042aa6d27255
Vinicius Soares <viniciussoares72943@gmail.com>
2022-05-14 23:10:20 -0300

    6 exercicio feito

--- fundamentos/bloco-04/dia-2/scripts.js
+++ fundamentos/bloco-04/dia-2/scripts.js
@@ -16,20 +16,36 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
  } */
 
 //EXERCICIO 3 
- 
+ /* 
  for (let index = 0; index < numbers.length; index +=1){
     resultado += numbers[index]; 
  }
 
-  let media = resultado / numbers.length;
+ let media = resultado / numbers.length;
    console.log(media);
-
+ */
 //EXERCICIO 4 
-
+/* 
  if (media > 20){
    console.log("valor maior que 20");
  }
  else{
    console.log("valor menor que 20");
+ } */
+
+//EXERCICIO 5
+
+//EXERCICIO 6 
+
+for (let index = 0; index < numbers.length; index +=1){
+  if (numbers[index] % 2 !== 0){ 
+    resultado += 1;
+  }
  }
  
+ if (resultado === 0){
+   console.log('nenhum valor ímpar encontrado')
+ }
+ else{
+   console.log(resultado)
+ }
