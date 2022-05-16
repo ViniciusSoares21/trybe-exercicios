880b21b49be39cbe80e2362c3831df785feba02e
Vinicius Soares <viniciussoares72943@gmail.com>
2022-05-16 10:18:03 -0300

    8, e 9 exercicios feitos

--- fundamentos/bloco-04/dia-2/scripts.js
+++ fundamentos/bloco-04/dia-2/scripts.js
@@ -34,17 +34,16 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
  } */
 
 //EXERCICIO 5
+/* 
+ let maiorNum = numbers[0];
 
-let maiorNum = numbers[0];
-
-for (let index = 1; index < numbers.length; index +=1){
+ for (let index = 1; index < numbers.length; index +=1){
   if (numbers[index] > maiorNum){
   maiorNum = numbers[index]
   }
-  
  }
  console.log(maiorNum)
-
+ */
 //EXERCICIO 6 
 /* 
 for (let index = 0; index < numbers.length; index +=1){
@@ -61,15 +60,30 @@ for (let index = 0; index < numbers.length; index +=1){
  }
  */ 
 
- //EXERCICIO 7
-
+//EXERCICIO 7
+/* 
  let menorNum = numbers[0];
 
-for (let index = 1; index < numbers.length; index +=1){
+ for (let index = 1; index < numbers.length; index +=1){
   if (numbers[index] < menorNum){
   menorNum = numbers[index]
   }
-  
  }
  console.log(menorNum)
+ */
 
+//EXERCICIO 8 
+
+ let exercicio8 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25]
+
+ for(let index2 = 0; index2 < exercicio8.length; index2 +=1){
+  console.log(exercicio8[index2]);
+ }
+
+//EXERCICIO 9 
+
+ for(let index2 = 0; index2 < exercicio8.length; index2 +=1){
+  console.log(exercicio8[index2] / 2);
+ }
+
+//FIM
