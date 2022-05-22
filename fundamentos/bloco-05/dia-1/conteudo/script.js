dfe2e43165b83d17fa4e9f1a890b39b17b63346e
Vinicius Soares <viniciussoares72943@gmail.com>
2022-05-22 16:57:03 -0300

    conteudos feitos

---
+++ fundamentos/bloco-05/dia-1/conteudo/script.js
@@ -0,0 +1,15 @@
+//TITULO
+document.getElementById("header-container").style.background = "MediumSeaGreen";
+
+//SECTION BACKGROUND
+document.querySelectorAll(".emergency-tasks")[0].style.background = "Salmon";
+document.querySelectorAll(".no-emergency-tasks")[0].style.background = "Gold";
+
+//SECTION TITLOS H3
+document.querySelectorAll("h3")[0].style.background = "Purple";
+document.querySelectorAll("h3")[1].style.background = "Purple";
+document.querySelectorAll("h3")[2].style.background = "black";
+document.querySelectorAll("h3")[3].style.background = "black";
+
+//RODAPE 
+document.getElementById("footer-container").style.background = "DarkSlateGray";
