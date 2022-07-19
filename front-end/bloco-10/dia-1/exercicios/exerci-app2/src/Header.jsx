85dc565ae2772096fdf317bb7d4c9987e0c59075
Vinicius Soares <viniciussoares72943@gmail.com>
2022-07-19 11:50:01 -0300

    exercicios feitos

---
+++ front-end/bloco-10/dia-1/exercicios/exerci-app2/src/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class nome extends React.Component {
+  render () {
+    return (
+      <div>
+        <h1 className='title'> Conteúdos de Front-End </h1>
+      </div>
+    );
+
+  }
+}
+
+export default nome;
