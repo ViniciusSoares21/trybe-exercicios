6b22d9e859486822031951c22f57e285154200ff
Vinicius Soares <viniciussoares72943@gmail.com>
2022-07-24 00:33:30 -0300

    cria filtro de pokemon por input text

--- front-end/bloco-11/dia-1/exercicios/my-pokedex/src/App.js
+++ front-end/bloco-11/dia-1/exercicios/my-pokedex/src/App.js
@@ -1,16 +1,55 @@
 import React from 'react';
 import './App.css';
 import pokemons from './data';
-//Import do json criado com os pokemons e seus dados.
-import Pokedex from './Pokedex';
 
 class App extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      filter: ''
+    }
+  }
+
+  getFilterValue = (event) => {
+    this.setState ({
+      filter: event.target.value
+    });
+  }
+
 render() {
-  return (
-    <div className="App">
-      <h1> Pokedex </h1>
-      <Pokedex pokemons={pokemons} />
+  const { filter } = this.state;
+
+  const filterPokemons = pokemons.filter((pokemonItem) => {
+    const pokemon = pokemonItem.name;
+    return pokemon.includes(filter)
+  })
+
+  const renderListHtml = filterPokemons.map((pokemonItem) => {
+    return <div className='pokemon' key={ pokemonItem.name}>
+      <div>
+      <p>{pokemonItem.name}</p>
+      <p>{pokemonItem.type}</p>
+      <p>
+      {`Average weight: ${pokemonItem.averageWeight.value} 
+      ${pokemonItem.averageWeight.measurementUnit}`}
+      </p>
+      </div>
+      <img src={ pokemonItem.image } alt={`${pokemonItem.name} vini`} /> 
+      
     </div>
+  });
+
+  return (
+    <main>
+      <header> 
+      <h1> Pokedex </h1>
+      <input onChange={this.getFilterValue} type="text" />
+      </header>
+      <div className='pokedex'>
+        { renderListHtml }
+      </div>
+    </main>
   );
 }
 
