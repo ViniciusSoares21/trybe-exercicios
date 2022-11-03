f5e3a1357a5767c4eaa60095e857f7a848ec8fde
Vinicius Soares <viniciussoares72943@gmail.com>
2022-11-03 13:57:12 -0300

    cria api rest com aplicação C.R.U.D utilizando fremawork Exprees

--- back-end/secao-4-node.js/dia-2/exercicio/src/app.js
+++ back-end/secao-4-node.js/dia-2/exercicio/src/app.js
@@ -1,5 +1,102 @@
 const express = require('express');
+const fs = require('fs').promises;
+const path = require('path');
+
+const moviesPath = path.resolve(__dirname, './movies.json');
 
 const app = express();
+
+app.use(express.json());
+
+async function fileReader() {
+  try {
+    const data = await fs.readFile(moviesPath, 'utf-8');
+    const movies = JSON.parse(data);
+    return movies;
+  } catch (err) {
+    console.error(`Erro ao ler o arquivo: ${err.message}`);
+  }
+}
+
+app.get('/movies/search', async (req, res) => {
+  try {
+    const { q } = req.query;
+    const movies = await fileReader();
+
+    if (q) {
+      const filteredMovies = movies.filter((element) => element.movie.includes(q));
+      return res.status(200).json(filteredMovies);
+    }
+    res.status(200).end();
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
+});
+
+app.get('/movies/:id', async (req, res) => {
+  try {
+    const movies = await fileReader();
+    const movie = movies.find(({ id }) => id === Number(req.params.id));
+    res.status(200).json(movie);
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+});
+
+app.get('/movies', async (req, res) => {
+  try {
+    const movies = await fileReader();
+    res.status(200).json(movies);
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+});
+
+app.post('/movies', async (req, res) => {
+  try {
+    const { movie, price } = req.body;
+    const movies = await fileReader();
+    const newMovie = {
+      id: movies[movies.length - 1].id + 1,
+      movie,
+      price,
+    };
+    const allMovies = JSON.stringify([...movies, newMovie]);
+    await fs.writeFile(moviesPath, allMovies);
+    res.status(201).json(newMovie);
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+});
+
+app.put('/movies/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { movie, price } = req.body;
+    const movies = await fileReader();
+    const index = movies.findIndex((element) => element.id === Number(id));
+    movies[index] = { id: Number(id), movie, price };
+    const updatedMovies = JSON.stringify(movies);
+    await fs.writeFile(moviesPath, updatedMovies);
+    res.status(200).json(movies[index]);
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
+});
+
+app.delete('/movies/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const movies = await fileReader();
+    const filteredMovies = movies.filter((movie) => movie.id !== Number(id));
+    const updatedMovies = JSON.stringify(filteredMovies);
+    await fs.writeFile(moviesPath, updatedMovies);
+    res.status(204).end();
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
+});
+
+
 
 module.exports = app;
